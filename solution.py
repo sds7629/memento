adec2bb6bc0c6ab47520aadafbdcf9f941f5aebd
sds7629 <sds7629@naver.com>
2024-09-30 19:14:26 +0900

    Feat: Solution

---
+++ solution.py
@@ -0,0 +1,21 @@
+# 1번 문제 x만큼 간격이 있는 n개의 숫자
+from typing import Sequence
+
+
+def solution1(x, n) -> Sequence[int]:
+    return [x * (i + 1) for i in range(n)]
+
+
+print(solution1(2, 5))
+print(solution1(4, 3))
+print(solution1(-4, 2))
+
+
+# 핸드폰 번호 가리기
+def solution2(phone_number) -> str:
+    back_number = phone_number[-4:]
+    return ("*" * (len(phone_number) - 4)) + back_number
+
+
+print(solution2("01033334444"))
+print(solution2("027778888"))
